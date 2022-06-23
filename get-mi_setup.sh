e3361a1bad7d50a00c01061b1388e94bf24cb544
bill <wbmurphey@gmail.com>
2022-06-23 13:07:19 -0600

    updated setup file

--- get-mi_setup.sh
+++ get-mi_setup.sh
@@ -1,2 +1,5 @@
 chmod +x ./get-mi.py
-sudo ln -s ./get-mi.py /usr/local/bin/get-mi
+mkdir $HOME/scripts 2>/dev/null && mkdir $HOME/scripts/Python 2>/dev/null
+cp ./get-mi.py $HOME/scripts/Python/get-mi.py
+sudo rm /usr/local/bin/get-mi
+sudo ln -s $HOME/scripts/Python/get-mi.py /usr/local/bin/get-mi
